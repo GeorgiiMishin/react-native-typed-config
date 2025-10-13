ebb944309bd839b8d093f0a6d8217ddb82d1b996
georgiy.mishin <georgiy.mishin@sbermarket.ru>
2025-10-13 16:07:01 +0300

    added desc

--- example/env/makeEnv.js
+++ example/env/makeEnv.js
@@ -1,8 +1,9 @@
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-module.exports = async function (platform, flavor) {
+/**
+ *
+ * @param {string} platform текущая платформа (android, ios)
+ * @param {string} buildTypeInfo тип сборки (для андроид - flavor + type, для ios - type)
+ */
+module.exports = async function (platform, buildTypeInfo) {
   await delay(5000);
   const result = await Promise.resolve({ platform, flavor, pidor: true });
 
